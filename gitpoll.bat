43c554d9a68469a719dfd22b9c63572d41f9afd6
Natotela <labthyneighbor@gmail.com>
2023-05-26 12:22:22 +0300

    %DATE% %TIME% GitPolled

--- gitpoll.bat
+++ gitpoll.bat
@@ -1,6 +1,7 @@
 git add -A
 git commit -m "%%DATE%% %%TIME%% GitPolled"
+git pull origin master
 git push origin master
 git merge upstream/master
 
-.\pull.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File ".\pull.ps1"
