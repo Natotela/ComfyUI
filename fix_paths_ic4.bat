0267469fcf2a4d9b71242bac5b76e66e3476159e
Natotela <labthyneighbor@gmail.com>
2023-05-26 11:54:19 +0300

    %%DATE%% %%TIME%% GitPolled submodules anime_seg

--- fix_paths_ic4.bat
+++ fix_paths_ic4.bat
@@ -1,6 +1,4 @@
-F:
-cd F:\Progz\Comfy\models
-
+cd models
 rmdir /s /q --ignore-fail-on-non-empty loras embeddings controlnet checkpoints vae style_models hypernetworks upscale_models
 
 mklink /j vae "F:\Progz\stable-diffusion-webui\models\VAE"
@@ -11,5 +9,5 @@ mklink /j checkpoints "F:\Progz\stable-diffusion-webui\models\Stable-diffusion"
 mklink /j style_models "F:\Progz\stable-diffusion-webui\models\ControlNet\T2I"
 mklink /j hypernetworks "F:\Progz\stable-diffusion-webui\models\hypernetworks"
 mklink /j upscale_models "F:\Progz\stable-diffusion-webui\models\ESRGAN"
-for %f in (F:\Progz\stable-diffusion-webui\models\RealESRGAN\*) do mklink F:\Progz\Comfy\models\upscale_models\%~nxf %f
-for %f in (F:\Progz\stable-diffusion-webui\models\SwinIR\*) do mklink F:\Progz\Comfy\models\upscale_models\%~nxf %f
+for %%f in (F:\Progz\stable-diffusion-webui\models\RealESRGAN\*) do mklink .\upscale_models\%%~nxf %%f
+for %%f in (F:\Progz\stable-diffusion-webui\models\SwinIR\*) do mklink .\upscale_models\%%~nxf %%f
--- gitpoll.bat
+++ gitpoll.bat
@@ -1,9 +1,11 @@
+git add -A
+git commit -m "%%DATE%% %%TIME%% GitPolled"
 git push origin master
 git merge upstream/master https://github.com/comfyanonymous/ComfyUI.git
 git fetch upstream
 git merge upstream/master
 
-for /d %%G in ("F:\progz\comfy\custom_nodes\*") do (
+for /d %%G in (".\custom_nodes\*") do (
     cd "%%G"
     git pull
 )
