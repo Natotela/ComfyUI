7e6bb5e4ac31617674d06294405ae302ea50029f
Natotela <labthyneighbor@gmail.com>
2023-05-26 12:19:28 +0300

    %DATE% %TIME% GitPolled

--- gitpoll.bat
+++ gitpoll.bat
@@ -1,8 +1,6 @@
 git add -A
 git commit -m "%%DATE%% %%TIME%% GitPolled"
 git push origin master
-git merge upstream/master https://github.com/comfyanonymous/ComfyUI.git
-git fetch upstream
 git merge upstream/master
 
 for /d %%G in (".\custom_nodes\*") do (
